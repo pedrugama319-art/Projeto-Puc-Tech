7c52743934689ea254631346c6b2cb5f853bbc74
Pedru Gama <pedrugama319@gmail.com>
2026-05-28 20:00:27 -0300

    fix: correçao de erros de logica

--- JogoDaVelha.cpp
+++ JogoDaVelha.cpp
@@ -7,27 +7,57 @@ int FVictory (char m[3][3]) //essa funcao so sera chamada a partir da quinta jog
 {
     int i, j;
 
-    for(i = 0; i < 3; i++)
+    for(i = 0; i < 3; i++)  //testa as vitorias na horizontal
     {
-        for(j = 0; j < 3; j++)
+        if(m[i][0] == m[i][1] && m[i][1] == m[i][2])
         {
-            if((m[i][0] == m[i][1] && m[i][1] == m[i][2]) ||
-            (m[0][j] == m[1][j] && m[1][j] == m[2][j]) || 
-            (m[0][0] == m[1][1] && m[1][1] == m[2][2]) ||
-            (m[0][2] == m[1][2] && m[1][2] == m[2][0]))
+            if(m[i][0] == 'X')
             {
-                if((m[i][0] == 'X') || (m[0][j] == 'X') || (m[0][0] == 'X') || (m[0][2] == 'X'))
-                {
-                    return 1;
-                }
-                else 
-                {
-                    return 2;
-                }
+                return 1;
             }
-
+            else
+            {
+                return 2;
+            }
         }
     }
+    for(j = 0; j < 3; j++)  //testa as vitorias na vertical
+    {
+        if(m[0][j] == m[1][j] && m[1][j] == m[2][j])
+        {
+            if(m[0][j] == 'X')
+            {
+                return 1;
+            }
+            else
+            {
+                return 2;
+            }
+        }
+    }
+    if(m[0][0] == m[1][1] && m[1][1] == m[2][2])    //testa a primeira vertical 
+    {
+        if(m[0][0] == 'X')
+        {
+            return 1;
+        }
+        else
+        {
+            return 2;
+        }
+    }
+    if(m[0][2] == m[1][1] && m[1][1] == m[2][0])    //testa a segunda vertical 
+    {
+        if(m[0][2] == 'X')
+        {
+            return 1;
+        }
+        else
+        {
+            return 2;
+        }
+    }
+
     
 }
 
