9e8d0b0c9cffde503535e6e11dae2dc6dd42bf65
LeopZ3006 <lozuchieri@gmail.com>
2026-05-30 08:50:02 -0300

    refactor: mudei a posição das chaves em alguns comandos

--- JogoDaVelha.cpp
+++ JogoDaVelha.cpp
@@ -30,57 +30,42 @@ void fTabuleiro(char tabuleiro[3][3]) {
         (tabuleiro[2][2] == ' ') ? '9' : tabuleiro[2][2]);
     }
 
-int FVictory (char m[3][3]) //essa funcao so sera chamada a partir da quinta jogada 
-{
+int FVictory (char m[3][3]){ //essa funcao so sera chamada a partir da quinta jogada 
     int i, j;
 
-    for(i = 0; i < 3; i++)  //testa as vitorias na horizontal
-    {
-        if(m[i][0] == m[i][1] && m[i][1] == m[i][2] && m[i][0] != ' ')
-        {
-            if(m[i][0] == 'X')
-            {
+    for(i = 0; i < 3; i++){  //testa as vitorias na horizontal
+        if(m[i][0] == m[i][1] && m[i][1] == m[i][2] && m[i][0] != ' '){
+            if(m[i][0] == 'X'){
                 return 1;
             }
-            else
-            {
+            else{
                 return 2;
             }
         }
     }
-    for(j = 0; j < 3; j++)  //testa as vitorias na vertical
-    {
-        if(m[0][j] == m[1][j] && m[1][j] == m[2][j] && m[0][j] != ' ')
-        {
-            if(m[0][j] == 'X')
-            {
+    for(j = 0; j < 3; j++){  //testa as vitorias na vertical
+        if(m[0][j] == m[1][j] && m[1][j] == m[2][j] && m[0][j] != ' '){
+            if(m[0][j] == 'X'){
                 return 1;
             }
-            else
-            {
+            else{
                 return 2;
             }
         }
     }
-    if(m[0][0] == m[1][1] && m[1][1] == m[2][2] && m[0][0] != ' ')    //testa a primeira diagonal
-    {
-        if(m[0][0] == 'X')
-        {
+    if(m[0][0] == m[1][1] && m[1][1] == m[2][2] && m[0][0] != ' '){    //testa a primeira diagonal
+        if(m[0][0] == 'X'){
             return 1;
         }
-        else
-        {
+        else{
             return 2;
         }
     }
-    if(m[0][2] == m[1][1] && m[1][1] == m[2][0] && m[0][2] != ' ')    //testa a segunda diagonal
-    {
-        if(m[0][2] == 'X')
-        {
+    if(m[0][2] == m[1][1] && m[1][1] == m[2][0] && m[0][2] != ' '){    //testa a segunda diagonal
+        if(m[0][2] == 'X'){
             return 1;
         }
-        else
-        {
+        else{
             return 2;
         }
     }
